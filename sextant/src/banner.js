9d5c717b6d9130597cf5803646b37897494e77f3
sachin <sachin22singh.ssa@gmail.com>
2023-08-24 14:03:46 +0530

    React

---
+++ sextant/src/banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './banner.css'; // Import your CSS or styling here
+
+const Banner = ({ title }) => {
+  return (
+    <div className="banner">
+      <h1>{"Monitoring for Technicians"}</h1>
+    </div>
+  );
+};
+
+export default Banner;
