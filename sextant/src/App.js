f59142759f2ae957a56973ea8940629f7894c31b
sachin <sachin22singh.ssa@gmail.com>
2023-08-28 14:27:10 +0530

    Latency

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
 import AddressDisplay from './AddressDisplay';
+import PylonConnector from './PylonConnector';
 
 class App extends Component {
     render() {
@@ -15,9 +16,13 @@ class App extends Component {
                 <Exhibit name="Public IPv6 Address">
                     <AddressDisplay url='https://api64.ipify.org?format=json' />
                 </Exhibit>
+                <Exhibit name="Pylon Packet Latency">
+                  <PylonConnector />
+                </Exhibit>
             </div>
         );
     }
 }
 
 export default App;
+
